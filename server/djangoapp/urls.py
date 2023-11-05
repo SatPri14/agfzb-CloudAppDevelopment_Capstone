47d80016a36ecce3e320f43b9c45032ff5581add
SatPri14 <khushbu.priyadarshni3@gmail.com>
2023-11-05 07:04:29 -0500

    Push changes for linting project
    Signed-off-by: SatPri14 <khushbu.priyadarshni3@gmail.com>

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-     path(route='', view=views.index, name='index'),
+     path(route='', view=views.index, name='home'),
 
     # path for about view
      path(route='about', view=views.about, name='about'),
@@ -17,12 +17,13 @@ urlpatterns = [
      path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
-
+    path(route='register', view=views.registration_request, name='register'),
     # path for login
-
+    path(route='login', view=views.login_request, name='login'),
     # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='dealer', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
