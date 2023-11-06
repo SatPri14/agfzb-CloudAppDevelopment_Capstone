7c49eb8a1f1518f3e1b310a8a81df4e05325a5e2
SatPri14 <khushbu.priyadarshni3@gmail.com>
2023-11-06 02:43:09 -0500

    Push changes for model
    Signed-off-by: SatPri14 <khushbu.priyadarshni3@gmail.com>

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
+admin.site.register(CarModel)
 
-# Register your models here.
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    fields = ['name', 'type', 'year', 'dealerid']
+    can_delete = False
+    show_change_link = True
+    extra = 0
 
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+   inlines = [CarModelInline,]
+   extra = 0
+
+# @admin.register(CarMake)
+# class CarMakeAdmin(admin.ModelAdmin):
+#     inlines = [CarModelInline]
+# admin.site.register(CarModel,CarMakeAdmin)
 # CarModelInline class
 
 # CarModelAdmin class
